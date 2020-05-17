ab62dded07102062c37a081bf6861527a258d852
Marcin Borysiak <marcinez24@gmail.com>
2020-05-17 11:59:49 +0200

    Cleared code, changed bracket style

--- main.c
+++ main.c
@@ -1,18 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <X11/Xlib.h>
+#include <stdbool.h>
 
-typedef struct Position {
+#define SHARED_KEY 2137
+
+typedef struct Position
+{
     unsigned int x;
     unsigned int y;
 } Position;
 
-typedef struct Car {
-    unsigned int x;
-    unsigned int y;
+typedef struct Car
+{
+    Position position;
 } Car;
 
-Position getMousePosition(XEvent xEvent) {
+//GLOBAL VARIABLES
+Display *display;
+
+Window window;
+
+GC gc;
+
+int screen;
+
+XEvent xEvent;
+
+XGCValues values;
+
+Car car;
+
+
+Position positionDifference(Position a, Position b)
+{
+
+    Position result;
+
+    result.x = a.x - b.x;
+    result.y = a.y - b.y;
+
+    return result;
+}
+
+bool comparePositions(Position a, Position b)
+{
+    if (a.x != b.x && a.y != b.y) {
+        return true;
+    }
+    return false;
+}
+
+Position getMousePosition(XEvent xEvent)
+{
 
     Position position;
 
@@ -21,70 +61,55 @@ Position getMousePosition(XEvent xEvent) {
 
     return position;
 }
-//to powinien byc raczej wspolbiezny proces
-void moveCar(Display *display, Window window, GC gc, int screen, Car car, Position position) {
 
-    int xDifference, yDifference;
+void moveCar(Display *display, Window window, GC gc, int screen, Car car, Position position)
+{
 
-    while (car.x != position.x && car.y != position.y) {
+    while (comparePositions(car.position, position))
+    {
+        
+        Position difference;
+        
+        difference = positionDifference(car.position, position);
 
-        xDifference = position.x - car.x;
-
-        yDifference = position.y - car.y;
-
-        if (xDifference > 0) {
-
-            car.x++;
-        } else {
-
-            car.x--;
+        if (difference.x > 0)
+        {
+            car.position.x++;
+        } 
+        else
+        {
+            car.position.x--;
         }
 
-        if (yDifference > 0) {
-
-            car.y++;
-        } else {
-
-            car.y--;
+        if (difference.y > 0)
+        {
+            car.position.y++;
+        } 
+        else
+        {
+            car.position.y--;
         }
     }
 }
 
-void drawCar(Display *display, Window window, GC gc, int screen, Car car) {
+void drawCar(Car car)
+{
+        int x = car.position.x;
+       
+        int y = car.position.y;
 
-        // Window childWindow = XCreateSimpleWindow(display, window,
-        //     car.xPosition, car.yPosition, 200, 100, 1, WhitePixel(display, screen), 
-        //     WhitePixel(display, screen));
         
-        // XSelectInput(display, childWindow, ExposureMask);
-
-        // XMapWindow(display, childWindow);
-
-        int x = car.x;
-        int y = car.y;
-
-        // XDrawRectangle(display, window, gc, 30, 70, 120, 30);
-
-        // XDrawRectangle(display, window, gc, 50, 50, 50, 20);
-
-        // XDrawArc(display, window, gc, 50-(30/2), 100-(30/2), 30, 30, 0, 360*64);
-
-        // XDrawArc(display, window, gc, 120-(30/2), 100-(30/2), 30, 30, 0, 360*64);
-
         XDrawRectangle(display, window, gc, x, y, 120, 30);
+        
         XDrawRectangle(display, window, gc, x + 20, y - 20, 50, 20);
+        
         XDrawArc(display, window, gc, (x + 20) - (30 / 2), (y + 30) - (30 / 2), 30, 30, 0, 360*64);
+        
         XDrawArc(display, window, gc, (x + 90) - (30 / 2), (y + 30) - (30 / 2), 30, 30, 0, 360*64);
-    }
+}
 
-int main (int argc, char *argv[]) {
-
-    Display *display;
-
-    Window window, childWindow;
-
-    GC gc;
-
+void init_display()
+{
     display = XOpenDisplay(NULL);
 
     if (display == NULL) {
@@ -93,12 +118,6 @@ int main (int argc, char *argv[]) {
 
         exit(1);
     }
-
-    int screen,
-        x,
-        y, 
-        width = 2,
-        height = 2;
 
     screen = DefaultScreen(display);
 
@@ -110,10 +129,6 @@ int main (int argc, char *argv[]) {
 
     XMapWindow(display, window);
 
-    XEvent xEvent;
-
-    XGCValues values;
-
     values.foreground = BlackPixel(display, screen);
     values.background = WhitePixel(display, screen);
 
@@ -121,64 +136,51 @@ int main (int argc, char *argv[]) {
 
     gc = XCreateGC(display, window, valuemask, &values);
 
-    if (gc < 0) {
+    if (gc < 0)
+    {
         fprintf(stderr, "XCreateGC: \n");
     }
-
-    unsigned int borderWidth,
-        depth;
-
-    Car car = {100, 100};
 
     XSetFillStyle(display, gc, FillSolid);
 
     XSetLineAttributes(display, gc, 2, LineSolid, CapRound, JoinRound);
-    
+
+}
+
+void eventLoop()
+{
     Position position;
 
-    // XGrabPointer(display, window, False, PointerMotionMask, GrabModeAsync,
-    //     GrabModeAsync, None, None, CurrentTime);
-
-    while (1) {
-        //drawCar(display, window, gc, screen, car);
-
+    while (true)
+    {
         XNextEvent(display, &xEvent);
 
         XClearWindow(display, window);
 
-        switch (xEvent.type) {
+        switch (xEvent.type)
+        {
             case Expose:
-                //drawCar(display, window, gc, screen, car);
                 break;
-            case MotionNotify:
 
+            case MotionNotify:
                 position = getMousePosition(xEvent);
 
-                car.x = position.x;
-                car.y = position.y;
+                car.position = position;
 
-                drawCar(display, window, gc, screen, car);
+                drawCar(car);
+
                 break;
             default:
                 break;
         }
-        // XDrawRectangle(display, window, gc, 30, 70, 120, 30);
-
-        // XDrawRectangle(display, window, gc, 50, 50, 50, 20);
-
-        // XDrawArc(display, window, gc, 50-(30/2), 100-(30/2), 30, 30, 0, 360*64);
-
-        // XDrawArc(display, window, gc, 120-(30/2), 100-(30/2), 30, 30, 0, 360*64);
-        // if (event.xany.window == childWindow && event.type == Expose) {
-
-        // }
-
-        //  if (xEvent.type == ButtonPress && event.xbutton.button == 1) {
-        //     XClearWindow(display, window);
-        //     car.x++;
-        //     drawCar(display, window, gc, screen, car);
-        // }
     }
+}
+
+int main (int argc, char *argv[])
+{
+    init_display();
+
+    eventLoop();
 
     return 0;
 }
