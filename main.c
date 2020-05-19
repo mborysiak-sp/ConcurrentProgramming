a37861cedba33f59fbee88dc33eca959df823bf3
Marcin Borysiak <marcinez24@gmail.com>
2020-05-19 17:59:03 +0200

    Changed project stucture

--- main.c
+++ main.c
@@ -12,7 +12,10 @@
 //MOZE NIE DZIALAC, ALE SPROBUJ
 
 
-#define SHARED_KEY 2137
+#define SHARED_KEY1 2137
+#define SHARED_KEY2 2138
+#define SHARED_KEY3 2139
+
 
 typedef struct Position
 {
@@ -25,6 +28,20 @@ typedef struct Player
     unsigned int id;
     Position position;
 } Player;
+
+typedef struct AppData
+{
+    Player* players;
+    unsigned int playerCount;
+    unsigned int nextPlayerID;
+} AppData;
+
+// typedef struct ApplicationState
+// {
+//     Player* players;
+//     int playerCount;
+//     int nextPlayerID;
+// } ApplicationState;
 
 //GLOBAL VARIABLES
 Display *display;
@@ -41,55 +58,75 @@ XGCValues values;
 
 Position car;
 
+int currentPlayerID;
 
 //SHARED GLOBAL VARIABLES
-Player* players;
+ AppData* appData;
+ int appDataID;
 
-int playersID;
+//  unsigned int* playerCount;
+//  int playerCountID;
 
-int playerCount;
+//  unsigned int* playerIterator;
+//  int playerIteratorID;
 
 
 //FUNCTIONS
 void initPlayers()
 {
-    playerCount = 0;
+    appData->playerCount = 0;
+    appData->nextPlayerID = 0;
+}
 
-    addPlayer();
+int assignID()
+{
+    int id = appData->nextPlayerID;
+    appData->nextPlayerID++;
+    return id;
 }
 
 void addPlayer()
 {
-    Player* temp = malloc((playerCount + 1) * sizeof(Player));
-
-    memcpy(temp, players, playerCount * sizeof(Player));
-
-    Player player = {playerCount, {0, 0}};
+    Player* temp = malloc(((appData->playerCount) + 1) * sizeof(Player));
     
-    temp[playerCount] = player;
+    memcpy(temp, appData->players, appData->playerCount * sizeof(Player));
 
-    players = temp;
+    // Player player = {0, {0, 0}};
 
-    playerCount++;
+    // player.id = assignID();
+
+    // currentPlayerID = player.id;
+    
+    // printf("%d", currentPlayerID);
+    
+    // temp[appData->playerCount] = player;
+
+    // appData->players = temp;
+
+    //free(temp);
+
+    appData->playerCount++;
 }
 
 void removePlayer(int id)
 {
-    Player* temp = malloc((playerCount - 1) * sizeof(Player));
+    Player* temp = malloc((appData->playerCount - 1) * sizeof(Player));
 
     if (id != 0)
     {
-        memcpy(temp, players, id * sizeof(Player));
+        memcpy(temp, appData->players, id * sizeof(Player));
     }
 
-    if (id != (playerCount - 1))
+    if (id != (appData->playerCount - 1))
     {
-        memcpy(temp + id, players + id + 1, (playerCount - id - 1) * sizeof(Player));
+        memcpy(temp + id, appData->players + id + 1, (appData->playerCount - id - 1) * sizeof(Player));
     }
 
-    players = temp;
+    appData->players = temp;
 
-    playerCount--;
+    free(temp);
+
+    appData->playerCount--;
 }
 
 Position positionDifference(Position a, Position b)
@@ -158,15 +195,15 @@ Position calculateDestination()
     
     int sumX, sumY;
 
-    for (int i = 0; i < playerCount; i++)
+    for (int i = 0; i < appData->playerCount; i++)
     {
-        Position playerPosition = players[i].position;
+        Position playerPosition = appData->players[i].position;
         sumX += playerPosition.x;
         sumY += playerPosition.y;
     }
 
-    sumX /= playerCount;
-    sumY /= playerCount;
+    sumX /= appData->playerCount;
+    sumY /= appData->playerCount;
 
     destination.x = sumX;
     destination.y = sumY;
@@ -231,32 +268,40 @@ void initDisplay()
 
 void initSharedMemory()
 {
-    if((playersID = shmget(SHARED_KEY, 1024, 0666 | IPC_CREAT | IPC_EXCL)) != -1)
+    if((appDataID = shmget(SHARED_KEY1, sizeof(AppData), 0666 | IPC_CREAT | IPC_EXCL)) != -1)
     {
+        //playerCountID = shmget(SHARED_KEY2, sizeof(unsigned int), 0666 | IPC_CREAT | IPC_EXCL);
+        //playerIteratorID = shmget(SHARED_KEY3, sizeof(unsigned int), 0666 | IPC_CREAT | IPC_EXCL);
+        //playerCount = shmat(playerCountID, 0, 0);
+        //playerIterator = shmat(playerIteratorID, 0, 0);
+        appData = (AppData*) shmat(appDataID, 0, 0);
         initPlayers();
-        printf("KUPA");
+        printf("pierwszy");
     }
     else
     {
-        addPlayer();
-        printf("DUPA");
-        playersID = shmget(SHARED_KEY, 1024, 0666 | IPC_CREAT);
+        printf("kolejny");
+        appDataID = shmget(SHARED_KEY1, sizeof(AppData), 0666 | IPC_CREAT);
+        appData = (AppData*) shmat(appDataID, 0, 0);
+        //playerCountID = shmget(SHARED_KEY2, sizeof(unsigned int), 0666 | IPC_CREAT);
+        //playerIteratorID = shmget(SHARED_KEY3, sizeof(unsigned int), 0666 | IPC_CREAT);
+        //playerCount = shmat(playerCountID, 0, 0);
+        //playerIterator = shmat(playerIteratorID, 0, 0);
     }
 
-    players = shmat(playersID, 0, 0);
+    addPlayer();
 }
 
 void clearSharedMemory()
 {
-    shmctl(playersID, IPC_RMID, 0);
-
+    shmctl(appDataID, IPC_RMID, 0);
+    //shmctl(playerCountID, IPC_RMID, 0);
+    //shmctl(playerIteratorID, IPC_RMID, 0);
     exit(0);
 }
 
 void eventLoop()
 {
-    Position position;
-
     while (true)
     {
         XNextEvent(display, &xEvent);
@@ -269,11 +314,11 @@ void eventLoop()
                 break;
 
             case MotionNotify:
-                position = getMousePosition(xEvent);
+                //appData->players[currentPlayerID].position = getMousePosition(xEvent);
 
-                car = position;
+                //car = calculateDestination();
 
-                drawCar(car);
+                //drawCar(car);
 
                 break;
             default:
